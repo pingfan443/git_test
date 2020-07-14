a9f0d94ab39e21e81007d26884dafe8c9e18d560
pingfan443 <pingfan23@foxmail.com>
2020-07-14 23:42:35 +0800

    测试代码

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#define repr(i,n) for(int i=1;i<=n;i++)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define pi pair<int>
+#define ll long long 
+#include<algorithm>
+#include<vector>
+#include<set>
+using namespace std;
+const int N=2e5+5;
+vector<int>cnts(26),cntt(26);
+set<string>res;
+int main()
+{
+	cout<<"hello world"; 
+ }
+ //赛后补出来了，需要去重 
